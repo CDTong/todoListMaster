1f3d00785e9cf6a81067f1d18433ec0da146a6c1
Tong <54443811+CDTong@users.noreply.github.com>
2020-04-21 03:59:14 +0800

    Add files via upload

--- src/main/java/com/axe/todo/controller/UserService.java
+++ src/main/java/com/axe/todo/controller/UserService.java
@@ -1,11 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-18 12:56:48
+ * @LastEditTime: 2020-04-21 03:53:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /undefined/Users/karro/IdeaProjects/todoList-master-clean/src/main/java/com/axe/todo/controller/UserService.java
+ */
 package com.axe.todo.controller;
 //控制器
 
-
+//push test
+import org.apache.catalina.filters.ExpiresFilter;
+import org.aspectj.apache.bcel.util.ClassLoaderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserService {
@@ -16,7 +31,21 @@ public class UserService {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView listAll() {
-        return new ModelAndView("login");
+    public ModelAndView loginCheck(HttpServletRequest request) {
+
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+
+        Map<String, String> data = new HashMap<>();
+        if("123".equals(user) && "123".equals(password)){
+            data.put("result", "登陆成功");
+        }
+        else{
+            data.put("result", "登陆失败");
+        }
+
+        return new ModelAndView("index");
     }
+
+
 }
